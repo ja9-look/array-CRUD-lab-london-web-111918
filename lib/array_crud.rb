fa3e50d31b156d0a6b2a98c9d7852e9253138bd6
Janine Luk <janineluk95@gmail.com>
2018-11-08 12:12:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
   array.unshift(element)
-
   array.unshift = ("element")
-
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +20,6 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-<<<<<<< HEAD
   array.shift
 end
 
